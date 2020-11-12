9539d78f2ba68c2e100dd81840046bce6cc4ad40
Daniel Schmitz <danieljfa@gmail.com>
2020-11-12 07:08:59 -0300

    menu da app dinamico

--- components/MenuApp.js
+++ components/MenuApp.js
@@ -1,17 +1,14 @@
 import { List, ListItem, ListItemText } from "@material-ui/core";
+import menu from "../menu";
 
 export default function MenuApp() {
     return (
         <List>
-            <ListItem button key="1" component="a" href="/">
-                <ListItemText primary="Home" />
-            </ListItem>
-            <ListItem button key="2" component="a" href="/page1">
-                <ListItemText primary="Page 1" />
-            </ListItem>
-            <ListItem button key="3" component="a" href="/page2">
-                <ListItemText primary="Page 2" />
-            </ListItem>
+            {menu.map(item => (
+                <ListItem button key={item.id} component="a" href={item.href}>
+                    <ListItemText primary={item.title} />
+                </ListItem>
+            ))}
         </List>
     )
 }
---
+++ menu.js
@@ -0,0 +1,6 @@
+const menu = [
+    {id:1, title: 'Home', href: '/'},
+    {id:2, title: 'Categories', href: '/categories'}
+]
+
+export default menu
