dd6d0f8630afe29c0c10f8450358131195b653bd
Daniel Schmitz <danieljfa@gmail.com>
2020-11-12 09:45:29 -0300

    DB singleton

--- db.js
+++ db.js
@@ -2,20 +2,32 @@ require('dotenv').config()
 const env = process.env.NODE_ENV || "development"
 const knexfile = require('./knexfile')
 
-let cachedConnection;
+const DB_KEY = Symbol.for("MyApp.db");
+const globalSymbols = Object.getOwnPropertySymbols(global);
+const hasDb = (globalSymbols.indexOf(DB_KEY) > -1);
 
 function getConnection() {
 
-    if (cachedConnection) {
-        console.log("Cached Connection");
-        return cachedConnection;
+    let connection;
+
+    if (!hasDb) {
+        console.log("New Connection");
+        connection = require('knex')(knexfile[env])
+        global[DB_KEY] = connection
+    } else {
+        console.log("Reuse Connection");
     }
 
-    console.log("New Connection");
-    const connection = require('knex')(knexfile[env])
-    cachedConnection = connection;
-    return connection;
+    const singleton = {};
+    Object.defineProperty(singleton, "instance", {
+        get: function () {
+            return global[DB_KEY];
+        }
+    });
+    Object.freeze(singleton);
+
+    return singleton;
 }
 
 const db = getConnection()
-module.exports = db
+module.exports = db.instance
