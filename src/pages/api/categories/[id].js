c0dbdfc27943722dd5963e14657c5ebb9afb7eeb
Daniel Schmitz <danieljfa@gmail.com>
2021-05-15 17:32:05 -0300

    refactor

--- src/pages/api/categories/[id].js
+++ src/pages/api/categories/[id].js
@@ -3,33 +3,34 @@ import categoriesService from '../../../services/categories'
 import HTTP_METHOD from '../../../utils/http_method'
 import HTTP_STATUS from '../../../utils/http_status'
 
-const api = async (req, res) => {
 
-    const {
-        query: { id },
-        method,
-        body
-    } = req
+export default handleErrors(
+    async (req, res) => {
 
-    if (method === HTTP_METHOD.GET) { // GET /api/categories/{id}
-        const result = await categoriesService.getById(id)
-        return res.status(HTTP_STATUS.OK).json(result)
+        const {
+            query: { id },
+            method,
+            body
+        } = req
+
+        if (method === HTTP_METHOD.GET) { // GET /api/categories/{id}
+            const result = await categoriesService.getById(id)
+            return res.status(HTTP_STATUS.OK).json(result)
+        }
+
+        if (method === HTTP_METHOD.DELETE) { // DELETE /api/categories/{id}
+            const result = await categoriesService.delete(id)
+            return res.status(HTTP_STATUS.OK).json(result)
+        }
+
+        if (method === HTTP_METHOD.PUT) { // PUT /api/categories/{id}
+            const { name, description } = body
+            const result = await categoriesService.update(id, name, description)
+            return res.status(HTTP_STATUS.OK).send(result)
+        }
+
+        // ANOTHERS METHODS... PATH FOR EXAMPLE....
+        return res.status(HTTP_STATUS.NOT_ALLOWED).send('Method Not Allowed')
+
     }
-
-    if (method === HTTP_METHOD.DELETE) { // DELETE /api/categories/{id}
-        const result = await categoriesService.delete(id)
-        return res.status(HTTP_STATUS.OK).json(result)
-    }
-
-    if (method === HTTP_METHOD.PUT) { // PUT /api/categories/{id}
-        const { name, description } = body
-        const result = await categoriesService.update(id, name, description)
-        return res.status(HTTP_STATUS.OK).send(result)
-    }
-
-    // ANOTHERS METHODS... PATH FOR EXAMPLE....
-    return res.status(HTTP_STATUS.NOT_ALLOWED).send('Method Not Allowed')
-
-}
-
-export default handleErrors(api)
+)
--- src/pages/api/categories/index.js
+++ src/pages/api/categories/index.js
@@ -4,19 +4,19 @@ import HTTP_METHOD from '../../../utils/http_method'
 import HTTP_STATUS from '../../../utils/http_status'
 
 
-const api = async (req, res) => {
+export default handleErrors(
+    async (req, res) => {
 
-    const { method, body } = req
+        const { method, body } = req
 
-    if (method === HTTP_METHOD.POST) {
-        const { name, description } = body
-        const result = await categoriesService.create(name, description)
-        return res.status(HTTP_STATUS.CREATED).json({ ...body, id: result[0] })
+        if (method === HTTP_METHOD.POST) {
+            const { name, description } = body
+            const result = await categoriesService.create(name, description)
+            return res.status(HTTP_STATUS.CREATED).json({ ...body, id: result[0] })
 
+        }
+
+        const result = await categoriesService.getAll()
+        return res.status(HTTP_STATUS.OK).json(result)
     }
-
-    const result = await categoriesService.getAll()
-    return res.status(HTTP_STATUS.OK).json(result)
-}
-
-export default handleErrors(api)
+)
