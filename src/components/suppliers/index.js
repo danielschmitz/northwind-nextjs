5564adf0bd4d69a9687e095c8062467579d54ce8
danielschmitz <danieljfa@gmail.com>
2021-05-23 19:14:53 -0300

    New Supplier Button

--- src/components/suppliers/index.js
+++ src/components/suppliers/index.js
@@ -2,62 +2,15 @@ import { useState } from "react"
 import { mutate } from "swr"
 import TableCategories from "./table"
 import GridCategories from "./grid"
-import FormCategories from "./form"
 import Confirm from "../confirm"
-import { Button, Hidden } from "@material-ui/core"
+import { Button, Hidden, Link } from "@material-ui/core"
 import http from "../../utils/http"
-import HTTP_METHOD from "../../utils/http_method"
 
 export default function Categories(props) {
-  const [open, setOpen] = useState(false)
   const [confirmDialogIsOpen, setConfirmDialogIsOpen] = useState(false)
   const [itemToDelete, setItemToDelete] = useState({})
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [formData, setFormData] = useState({})
 
-  const handleOpen = () => {
-    setOpen(true)
-  }
-
-  const handleClose = () => {
-    setError(false)
-    setOpen(false)
-  }
-
-  const handleNew = () => {
-    setFormData({
-      name: "",
-      description: "",
-    })
-    handleOpen()
-  }
-
-  const handleEdit = (row) => {
-    setFormData(row)
-    handleOpen()
-  }
-
-  const handleSave = async (data) => {
-    console.log("save", data)
-    setError(false)
-    setLoading(true)
-    try {
-      await http({
-        method: data.id ? HTTP_METHOD.PUT : HTTP_METHOD.POST,
-        url: data.id ? `/api/categories/${data.id}` : "/api/categories",
-        data,
-      })
-      refreshData()
-      setLoading(false)
-      handleClose()
-    } catch (error) {
-      setLoading(false)
-      if (error.response && error.response.data) {
-        setError(error.response.data)
-      }
-    }
-  }
+  const handleEdit = (row) => {}
 
   const handleDelete = async (data) => {
     setItemToDelete(data)
@@ -76,11 +29,18 @@ export default function Categories(props) {
   }
 
   const refreshData = () => {
-    mutate("/api/categories", (data) => data)
+    mutate("/api/suppliers", (data) => data)
   }
 
   return (
     <>
+      <Link href="/suppliers/new">
+        <Button size="small" color="primary">
+          New Supplier
+        </Button>
+      </Link>
+      <br />
+      <br />
       <Hidden smDown>
         <TableCategories
           onEdit={handleEdit}
@@ -93,25 +53,14 @@ export default function Categories(props) {
           onDelete={handleDelete}
         ></GridCategories>
       </Hidden>
-      <FormCategories
-        open={open}
-        onClose={handleClose}
-        onSave={handleSave}
-        onDelete={handleDelete}
-        formData={formData}
-        error={error}
-        loading={loading}
-      ></FormCategories>
       <br />
-      <Button size="small" color="primary" onClick={handleNew}>
-        New Supplier
-      </Button>
+
       <Confirm
         open={confirmDialogIsOpen}
         onOk={handleDeleteOk}
         onCancel={() => setConfirmDialogIsOpen(false)}
       >
-        Confirm delete supplier <i>{itemToDelete.name}</i> ?
+        Confirm delete supplier <i>{itemToDelete.contactName}</i> ?
       </Confirm>
     </>
   )
---
+++ src/pages/suppliers/new.js
@@ -0,0 +1,3 @@
+export default function NewSupplierPage(props) {
+  return <div>New Supplier Page</div>
+}
