14080d080f7ca240b998393d7c1ef70e054ff0d7
Daniel Schmitz <danieljfa@gmail.com>
2021-05-29 07:13:28 -0300

    revert dark theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,11 @@
-import React, { useState } from "react"
-
 import TopBar from "../components/topbar"
 import "../styles/globals.css"
 
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles"
-import Switch from "@material-ui/core/Switch"
-
 function MyApp({ Component, pageProps }) {
-  const [darkState, setDarkState] = useState(true)
-  const palletType = darkState ? "dark" : "light"
-  const darkTheme = createMuiTheme({
-    palette: {
-      type: palletType,
-    },
-  })
-
-  const handleThemeChange = () => {
-    setDarkState(!darkState)
-  }
-
   return (
     <>
-      <MuiThemeProvider theme={darkTheme}>
-        <Switch checked={darkState} onChange={handleThemeChange} />
-        <TopBar titulo="Northwind App"></TopBar>
-        <Component {...pageProps} />
-      </MuiThemeProvider>
+      <TopBar titulo="Northwind App"></TopBar>
+      <Component {...pageProps} />
     </>
   )
 }
