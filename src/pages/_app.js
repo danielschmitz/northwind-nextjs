db888fbbf939c7b6d7662a4a0a48f84601c970e0
Daniel Schmitz <danieljfa@gmail.com>
2021-05-29 07:02:39 -0300

    dark theme test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,31 @@
+import React, { useState } from "react"
+
 import TopBar from "../components/topbar"
 import "../styles/globals.css"
 
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles"
+import Switch from "@material-ui/core/Switch"
+
 function MyApp({ Component, pageProps }) {
+  const [darkState, setDarkState] = useState(true)
+  const palletType = darkState ? "dark" : "light"
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+    },
+  })
+
+  const handleThemeChange = () => {
+    setDarkState(!darkState)
+  }
+
   return (
     <>
-      <TopBar titulo="Northwind App"></TopBar>
-      <Component {...pageProps} />
+      <MuiThemeProvider theme={darkTheme}>
+        <Switch checked={darkState} onChange={handleThemeChange} />
+        <TopBar titulo="Northwind App"></TopBar>
+        <Component {...pageProps} />
+      </MuiThemeProvider>
     </>
   )
 }
